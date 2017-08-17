8ed9b4d6c4850f3a0bd700257f6d5eb97e1e8c1c
Miko Dagatan <miguel.dagatan@gmail.com>
2017-08-18 01:52:02 +0800

    removed leave_request_id in employments

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20170817073956) do
     t.integer "company_id"
     t.integer "user_id"
     t.integer "role_id"
-    t.integer "leave_request_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
