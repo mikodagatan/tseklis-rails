5a6c48bb41bccecf27bfc9171ee38a994ea6a537
Miko Dagatan <miguel.dagatan@gmail.com>
2017-08-18 12:03:30 +0800

    added validations to registration

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
 belongs_to :company
+
+validates :first_line, presence: { message: "Please enter the First line"}
+
+validates :second_line, presence: { message: "Please enter the Second line"}
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
 belongs_to :user
+
+validates_presence_of :first_name
+validates_presence_of :last_name
 end
