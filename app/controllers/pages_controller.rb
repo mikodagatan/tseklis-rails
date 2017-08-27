b3175e5aee407be5354ef133784012ce5a270da2
Miko Dagatan <miguel.dagatan@gmail.com>
2017-08-27 12:55:56 +0800

    Added landing page image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,14 @@ class PagesController < ApplicationController
 	before_action :set_up
   
   def home
-  	if current_user.leave_amounts.present?
-  		# to change for multiple companies
-  		@company = @current_user.employments.last.company
-	  	@leaves = []
-			@month_segmented_leaves = current_user.segmented_leaves(Date.today.all_month, @company)
-			@month_total_leaves = current_user.total_leaves(Date.today.all_month, @company)
+  	if user_signed_in?
+	  	if current_user.leave_amounts.present?
+	  		# to change for multiple companies
+	  		@company = @current_user.employments.last.company
+		  	@leaves = []
+				@month_segmented_leaves = current_user.segmented_leaves(Date.today.all_month, @company)
+				@month_total_leaves = current_user.total_leaves(Date.today.all_month, @company)
+			end
 		end
   end
 
