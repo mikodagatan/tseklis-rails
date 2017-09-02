eddd0da1fdd4c297c1ee6394dedf0ed256ca2abf
Miko Dagatan <miguel.dagatan@gmail.com>
2017-09-03 07:36:47 +0800

    added gemfiles for development.

--- Gemfile
+++ Gemfile
@@ -14,18 +14,25 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'listen', '>= 3.0.5', '< 3.2'
+gem 'elasticsearch'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'factory_girl_rails'
+  gem 'rspec-rails', '~> 3.6'
 end
 
 group :development do
   gem 'web-console', '>= 3.3.0'
-
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem 'pry-rails'
+  gem "letter_opener"
+
 end
 
 group :production do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
@@ -40,10 +40,13 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  # Letter Opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
