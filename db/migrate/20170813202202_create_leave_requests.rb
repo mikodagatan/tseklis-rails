4bacb1dc18053aaaca27368a71fc758da2e4dc3e
Miko Dagatan <miguel.dagatan@gmail.com>
2017-09-05 17:39:02 +0800

    Removed reviewed column

--- db/migrate/20170813202202_create_leave_requests.rb
+++ db/migrate/20170813202202_create_leave_requests.rb
@@ -4,14 +4,13 @@ class CreateLeaveRequests < ActiveRecord::Migration[5.1]
     	t.string    :title
     	t.text 		:description
     	t.date 		:start_date
-        t.time      :start_time
+      t.time      :start_time
     	t.date 		:end_date
-        t.time      :end_time
-        t.boolean   :acceptance
-        t.integer   :acceptor_id
-        t.boolean   :reviewed
-        t.integer   :leave_type_id
- 
+      t.time      :end_time
+      t.boolean   :acceptance
+      t.integer   :acceptor_id
+      t.integer   :leave_type_id
+
     	t.integer   :employment_id
 
 
