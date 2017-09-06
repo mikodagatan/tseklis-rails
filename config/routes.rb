fdc48cf247d3ef1aa1a6599c479ef6f47b8477d2
Miko Dagatan <miguel.dagatan@gmail.com>
2017-09-06 22:43:54 +0800

    added dashboard controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   devise_scope :user do
-  	get '/users/sign_out' => 'devise/sessions#destroy' 
+  	get '/users/sign_out' => 'devise/sessions#destroy'
 	end
-
 
   resources :users do
   	resource :profile
@@ -12,12 +11,26 @@ Rails.application.routes.draw do
       resources :leave_requests
     end
   end
-  
+
   resources :companies do
     resources :leave_types
     resources :holidays
   end
-  
+
+
+  namespace :dashboard do
+    resources :contact_message
+    resources :endorsements
+    resources :features
+    resources :owners
+    resources :plans
+    resources :roles
+    resources :testimonials
+    resource :site_setting
+    resource :landing_page_setting
+    resource :user_home_page_setting
+  end
+
 	get 'about' => 'pages#about'
 	root 'pages#home'
 end
