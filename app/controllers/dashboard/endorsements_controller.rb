44138b4c2e8e5fa77dd01128d0112ac38948f815
Miko Dagatan <miguel.dagatan@gmail.com>
2017-09-07 11:59:56 +0800

    added views for endorsements

--- app/controllers/dashboard/endorsements_controller.rb
+++ app/controllers/dashboard/endorsements_controller.rb
@@ -1,2 +1,14 @@
 class Dashboard::EndorsementsController < Dashboard::ResourceController
+
+  def endorsement_params
+  	params.require(:endorsement).permit(
+            :id,
+            :name,
+            :photo,
+            :link,
+            :award_name,
+            :rating,
+            :description,
+            )
+  end
 end
--- app/helpers/dashboard/application_helper.rb
+++ app/helpers/dashboard/application_helper.rb
@@ -1,2 +1,6 @@
 module Dashboard::ApplicationHelper
+
+  def better_file_size(file_size)
+    number_with_delimiter(file_size, delimiter: ',', precision: 2)
+  end
 end
