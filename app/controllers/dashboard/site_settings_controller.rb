933e15e7b30ecb7a5127d9fd594eb15faced4669
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-09-18 16:26:21 +0800

    fixed problems with initializing website

--- app/controllers/dashboard/site_settings_controller.rb
+++ app/controllers/dashboard/site_settings_controller.rb
@@ -2,7 +2,7 @@ class Dashboard::SiteSettingsController < Dashboard::ResourceController
 
   def create
     site_setting = SiteSetting.new(site_setting_params)
-    if site_setting = site_setting.save
+    if site_setting.save
       redirect_to edit_dashboard_site_setting_url(site_setting)
     else
       render action: :new
