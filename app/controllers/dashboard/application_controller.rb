4a56fc838302d9063f8d6b0c3de1ca2eddb10a14
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-09-22 13:14:06 +0800

    changed password for admin

--- app/controllers/dashboard/application_controller.rb
+++ app/controllers/dashboard/application_controller.rb
@@ -2,5 +2,5 @@ class Dashboard::ApplicationController < ActionController::Base
 
   protect_from_forgery
   protected
-  http_basic_authenticate_with name: "admin", password: "!tseklisadministratorFragilesilence0"
+  http_basic_authenticate_with name: "admin", password: "!tseklisadministrator65fb35f526"
 end
