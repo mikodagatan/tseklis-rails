58b1854406bcdaa71b4d9214556ed51fc7bd242b
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-09-28 19:01:00 +0800

    @leave_request to :leave_request at form

--- db/migrate/20170907142534_change_column_active_on_testimonials.rb
+++ db/migrate/20170907142534_change_column_active_on_testimonials.rb
@@ -1,5 +1,9 @@
 class ChangeColumnActiveOnTestimonials < ActiveRecord::Migration[5.1]
-  def change
+  def self.up
     change_column :testimonials, :active, :boolean, default: true
+  end
+
+  def self.down
+    change_column :testimonials, :active, :boolean
   end
 end
