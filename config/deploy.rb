ccc5db0ca51aef3f9af6dc922c150d1536870e40
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-10-03 03:27:13 +0800

    added console in cap production

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,18 @@ set :deploy_to, "/home/deploy/tseklis"
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+namespace :rails do
+  desc 'Open a rails console `cap [staging] rails:console [server_index default: 0]`'
+  task :console do
+    server = roles(:app)[ARGV[2].to_i]
+
+    puts "Opening a console on: #{server.hostname}...."
+
+    cmd = "ssh #{server.user}@#{server.hostname} -t 'cd #{fetch(:deploy_to)}/current && RAILS_ENV=#{fetch(:rails_env)} bundle exec rails console'"
+
+    puts cmd
+
+    exec cmd
+  end
+end
