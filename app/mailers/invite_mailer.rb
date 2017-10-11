097165ac0fa882991ed775b3ac79df098dfd1db1
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-10-12 02:01:46 +0800

    removed capitalization

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -4,13 +4,13 @@ class InviteMailer < ApplicationMailer
     @invite = invite
     @url  = url
     @sender = User.find(invite.sender_id)
-    mail(to: @invite.email, subject: @sender.profile.first_name.capitalize + " " + @sender.profile.last_name.capitalize + " invites you to use Tseklis" )
+    mail(to: @invite.email, subject: @sender.profile.first_name + " " + @sender.profile.last_name + " invites you to use Tseklis" )
   end
 
   def existing_user_invite(invite)
     @invite = invite
     @sender = User.find(invite.sender_id)
-    mail(to: @invite.email, subject: @sender.profile.first_name.capitalize + " " + @sender.profile.last_name.capitalize + " invites you to join " + @invite.company.name )
+    mail(to: @invite.email, subject: @sender.profile.first_name + " " + @sender.profile.last_name + " invites you to join " + @invite.company.name )
   end
 
 
