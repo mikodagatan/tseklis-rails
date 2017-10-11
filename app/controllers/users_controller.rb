3ee9bd8e6db8f762b9f2fab8121dc5ffbde6f9e6
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-10-12 00:49:53 +0800

    fixed difference between edit account and other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_up, only: :show
+  before_action :set_up, only: [:show, :edit]
 
   def show
     @user = User.find(params[:id])
