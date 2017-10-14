4ad8f14fa03c35202775c7605092dc1c0a5a6d27
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2017-10-14 13:50:20 +0800

    added notifications in the navbar. incomplete

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
-  belongs_to :user
-  belongs_to :acting_user, class_name: 'User'
-  belongs_to :employment
+  belongs_to :user, required: false
+  belongs_to :acting_user, class_name: 'User', required: false
+  belongs_to :employment, required: false
   belongs_to :leave_request, required: false
 
   validates :user_id, :acting_user_id, :employment_id, :notice_type, presence: true
