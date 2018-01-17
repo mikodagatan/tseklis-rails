da92a87f393f67ee4225b223bd1a88a891576f41
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-01-17 13:39:03 +0800

    changed deploy precompile asset script

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,48 +57,16 @@ namespace :rails do
 
   set :rbenv_ruby, '2.4.0'
 
-  # set the locations that we will look for changed assets to determine whether to precompile
-  set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
-
-  # # clear the previous precompile task
-  # Rake::Task["deploy:assets:precompile"].clear_actions
-  # class PrecompileRequired < StandardError; end
-  #
-  # namespace :deploy do
-  #   namespace :assets do
-  #     desc "Precompile assets"
-  #     task :precompile do
-  #       on roles(fetch(:assets_roles)) do
-  #         within release_path do
-  #           with rails_env: fetch(:rails_env) do
-  #             begin
-  #               # find the most recent release
-  #               latest_release = capture(:ls, '-xr', releases_path).split[1]
-  #
-  #               # precompile if this is the first deploy
-  #               raise PrecompileRequired unless latest_release
-  #
-  #               latest_release_path = releases_path.join(latest_release)
-  #
-  #               # precompile if the previous deploy failed to finish precompiling
-  #               execute(:ls, latest_release_path.join('assets_manifest_backup')) rescue raise(PrecompileRequired)
-  #
-  #               fetch(:assets_dependencies).each do |dep|
-  #                 # execute raises if there is a diff
-  #                 execute(:diff, '-Naur', release_path.join(dep), latest_release_path.join(dep)) rescue raise(PrecompileRequired)
-  #               end
-  #
-  #               info("Skipping asset precompile, no asset diff found")
-  #
-  #               # copy over all of the assets from the last release
-  #               execute(:cp, '-r', latest_release_path.join('public', fetch(:assets_prefix)), release_path.join('public', fetch(:assets_prefix)))
-  #             rescue PrecompileRequired
-  #               execute(:rake, "assets:precompile")
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
+  namespace :deploy do
+    namespace :assets do
+      task :precompile, :roles => :web, :except => { :no_release => true } do
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+      end
+    end
+  end
 end
