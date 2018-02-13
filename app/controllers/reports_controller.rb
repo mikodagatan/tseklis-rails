10576c330fd14b2511715487b8f63f2b75906f8c
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-02-13 23:35:20 +0800

    added excel export to Reports: remaining leaves

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,10 +6,19 @@ class ReportsController < ApplicationController
   end
 
   def remaining_leaves
-    @r_date = re_params[:date_as_of].to_date unless params[:re].blank?
+    @r_date = re_params unless params[:re].blank?
+    @r_date2 = @r_date[:date_as_of].to_date
     @remaining = User.joins(:employments).where(employments: {company: @company, end_date: nil})
-    unless @r_date.blank?
-      @remaining = User.joins(:employments).where(employments: {company: @company}).where('employments.end_date > ? or employments.end_date is ?', @r_date, nil)
+    unless @r_date2.blank?
+      @remaining = User.joins(:employments).where(employments: {company: @company}).where('employments.end_date > ? or employments.end_date is ?', @r_date2, nil)
+    end
+
+    respond_to do |format|
+      format.html
+      format.xlsx {
+        @remaining = @remaining
+        @r_date2 = @r_date2
+      }
     end
   end
 
