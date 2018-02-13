4612d49deac1fd1bf51ad3a5b137d7d95da34171
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-02-13 22:38:47 +0800

    changed search to do search by pressing enter

--- app/assets/javascripts/employment_search.js
+++ app/assets/javascripts/employment_search.js
@@ -1,14 +1,14 @@
 $(document).on('turbolinks:load', function() {
-  $('#employments_search input').keyup( function() {
-    $.get($("#employments_search").attr("action"), $("#employments_search").serialize(), null, "script");
-    return false;
-
-  });
-  $('#employments_search input').bind('keypress', function(e)
-    {
-      if(e.keyCode == 13)
-      {
-      return false;
-      }
-    });
+  // $('#employments_search input').keyup( function() {
+  //   $.get($("#employments_search").attr("action"), $("#employments_search").serialize(), null, "script");
+  //   return false;
+  //
+  // });
+  // $('#employments_search input').bind('keypress', function(e)
+  //   {
+  //     if(e.keyCode == 13)
+  //     {
+  //     return false;
+  //     }
+  //   });
 });
