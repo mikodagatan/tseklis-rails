40f6cb0c72f251252f3553ca49d2765ed7df13f1
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-03-01 16:03:58 +0800

    removed departments and projects

--- app/models/time_request.rb
+++ app/models/time_request.rb
@@ -2,5 +2,6 @@ class TimeRequest < ApplicationRecord
   belongs_to :employment
 
   # validates :time_registered, format: { with: /([01]?[0-9]|2[0-3]):[0-5][0-9]/, message: "Please enter a 24-hour time format. Example: '13:21' for 1:21 pm" }
-  
+
+  validates :date, :time, :in_out, presence: true
 end
